f7348055053316db8b5f79734be12c5f3f4046f9
Tresor-ilunga <19im065@esisalama.org>
2023-07-20 21:44:12 +0200

    fix(app) Remove Docker and add The factories

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ *
+ * @author tresor-ilunga <ilungat82@gmail.com>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(),
+            'content' => $this->faker->paragraph(),
+            'crated_at' => now(),
+        ];
+    }
+}
